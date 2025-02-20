20e540ce9d7de3119c8536b349b5347aa01ce832
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2025-02-20 16:14:36 +0300

    Update crud.py

--- app/app_crud/crud.py
+++ app/app_crud/crud.py
@@ -2,11 +2,14 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.app_models import models
 from app.app_schemas import schemas
+from app.app_services.services import services_add_referral_code, services_add_user
+
 
 async def get_user_by_email(db: AsyncSession, email: str):
     query = select(models.User).where(models.User.email == email)
     result = await db.execute(query)
     return result.scalars().first()
+
 
 async def create_user(db: AsyncSession, user: schemas.UserCreate):
     hashed_password = await services_add_user(user.hashed_password)
@@ -20,8 +23,10 @@ async def create_user(db: AsyncSession, user: schemas.UserCreate):
     await db.refresh(db_user)
     return db_user
 
+
 async def create_referral_code(db: AsyncSession, user_id: int, expiration_days: int):
     return await services_add_referral_code(db, user_id, expiration_days)
+
 
 async def delete_referral_code(db: AsyncSession, user_id: int):
     result = await db.execute(
@@ -30,9 +35,10 @@ async def delete_referral_code(db: AsyncSession, user_id: int):
     referral_code = result.scalar_one_or_none()
     if not referral_code:
         raise Exception("Нет активного реферального кода")
-    
+
     await db.delete(referral_code)
     await db.commit()
+
 
 async def get_referral_code_by_email(db: AsyncSession, email: str):
     user = await get_user_by_email(db, email)
@@ -41,6 +47,7 @@ async def get_referral_code_by_email(db: AsyncSession, email: str):
             select(models.ReferralCode).where(models.ReferralCode.user_id == user.id)
         ).scalar_one_or_none()
     return None
+
 
 async def get_referrals_by_referrer_id(db: AsyncSession, referrer_id: int):
     referrals = await db.execute(
