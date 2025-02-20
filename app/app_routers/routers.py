e906bf9cbabdbb58ac537895f36e42711d8f230c
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2025-02-20 16:14:23 +0300

    Update routers.py

--- app/app_routers/routers.py
+++ app/app_routers/routers.py
@@ -3,20 +3,24 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
+
+from app.app_auth import auth
 from app.app_auth.auth import authenticate_user, create_access_token
 from app.app_crud import crud
 from app.app_database.database import get_db
 from app.app_models import models
 from app.app_schemas import schemas
-from app.app_services.user_services import services_user_check
+from app.app_services.services import services_user_check
 
 user_router = APIRouter()
+
 
 @user_router.post("/register", response_model=schemas.UserResponse)
 async def register(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
     await services_user_check(user)
     new_user = await crud.register_user(db, user)
     return new_user
+
 
 @user_router.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
@@ -26,14 +30,18 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSessi
     access_token = await create_access_token(data={"sub": user.user})
     return {"token_type": "bearer", "access_token": access_token}
 
+
 @user_router.post("/referral-code", response_model=schemas.ReferralCodeResponse)
-async def create_referral_code(expiration_days: int, db: AsyncSession = Depends(get_db), user: models.User = Depends(auth.get_current_user)):
+async def create_referral_code(expiration_days: int, db: AsyncSession = Depends(get_db),
+                               user: models.User = Depends(auth.get_current_user)):
     new_code = await crud.create_referral_code(db, user.id, expiration_days)
     return new_code
+
 
 @user_router.delete("/referral-code", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_referral_code(db: AsyncSession = Depends(get_db), user: models.User = Depends(auth.get_current_user)):
     await crud.delete_referral_code(db, user.id)
+
 
 @user_router.get("/referral-code-by-email/{email}", response_model=schemas.ReferralCodeResponse)
 async def get_referral_code_by_email(email: str, db: AsyncSession = Depends(get_db)):
@@ -41,6 +49,7 @@ async def get_referral_code_by_email(email: str, db: AsyncSession = Depends(get_
     if not referral_code:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Referral code not found")
     return referral_code
+
 
 @user_router.get("/referrals/{referrer_id}", response_model=List[schemas.ReferralResponse])
 async def get_referrals(referrer_id: int, db: AsyncSession = Depends(get_db)):
